e5e913d19bda3f56e4e5ec4257222e96ddf9fb9b
kadir <akadirkkoc@gmail.com>
2020-04-24 22:06:22 +0300

    Fix unit tests

--- merge-sort/src/test/java/io/github/akadir/MergeSortTest.java
+++ merge-sort/src/test/java/io/github/akadir/MergeSortTest.java
@@ -2,6 +2,7 @@ package io.github.akadir;
 
 import io.github.akadir.mergesort.MergeSort;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -10,24 +11,64 @@ import org.junit.Test;
  * Time: 21:32
  */
 public class MergeSortTest {
-    private final MergeSort mergeSort = new MergeSort();
+    private MergeSort mergeSort;
+
+    @Before
+    public void initialize() {
+        mergeSort = new MergeSort();
+    }
 
     @Test
-    public void mergeSortTest() {
-        int[] ascOrdered = {1, 2, 3, 4, 5};
-        int[] descOrdered = {5, 4, 3, 2, 1};
-        int[] sameElements = {1, 1, 1, 1, 1};
-        int[] empty = {};
+    public void mergeSortTestAscDesc() {
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] expected = {5, 4, 3, 2, 1};
+        Assert.assertArrayEquals(expected, mergeSort.sort(arr, MergeSort.SortOrder.DESC));
+    }
 
-        Assert.assertArrayEquals(ascOrdered, mergeSort.sort(descOrdered, MergeSort.SortOrder.ASC));
-        Assert.assertArrayEquals(descOrdered, mergeSort.sort(descOrdered, MergeSort.SortOrder.DESC));
-        Assert.assertArrayEquals(descOrdered, mergeSort.sort(ascOrdered, MergeSort.SortOrder.DESC));
-        Assert.assertArrayEquals(ascOrdered, mergeSort.sort(ascOrdered, MergeSort.SortOrder.ASC));
-        Assert.assertArrayEquals(sameElements, mergeSort.sort(sameElements, MergeSort.SortOrder.ASC));
-        Assert.assertArrayEquals(sameElements, mergeSort.sort(sameElements, MergeSort.SortOrder.DESC));
-        Assert.assertArrayEquals(empty, mergeSort.sort(empty, MergeSort.SortOrder.ASC));
-        Assert.assertArrayEquals(empty, mergeSort.sort(empty, MergeSort.SortOrder.DESC));
-        Assert.assertArrayEquals(null, mergeSort.sort(null, MergeSort.SortOrder.DESC));
+    @Test
+    public void mergeSortTestAscAsc() {
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] expected = {1, 2, 3, 4, 5};
+        Assert.assertArrayEquals(expected, mergeSort.sort(arr, MergeSort.SortOrder.ASC));
+    }
+
+    @Test
+    public void mergeSortTestDescAsc() {
+        int[] arr = {5, 4, 3, 2, 1};
+        int[] expected = {1, 2, 3, 4, 5};
+        Assert.assertArrayEquals(expected, mergeSort.sort(arr, MergeSort.SortOrder.ASC));
+    }
+
+    @Test
+    public void mergeSortTestDescDesc() {
+        int[] arr = {5, 4, 3, 2, 1};
+        int[] expected = {5, 4, 3, 2, 1};
+        Assert.assertArrayEquals(expected, mergeSort.sort(arr, MergeSort.SortOrder.DESC));
+    }
+
+    @Test
+    public void mergeSortTestEmptyDesc() {
+        int[] arr = {};
+        int[] expected = {};
+        Assert.assertArrayEquals(expected, mergeSort.sort(arr, MergeSort.SortOrder.DESC));
+    }
+
+    @Test
+    public void mergeSortTestEmptyAsc() {
+        int[] arr = {};
+        int[] expected = {};
+        Assert.assertArrayEquals(expected, mergeSort.sort(arr, MergeSort.SortOrder.ASC));
+    }
+
+    @Test
+    public void mergeSortTestNullAsc() {
         Assert.assertArrayEquals(null, mergeSort.sort(null, MergeSort.SortOrder.ASC));
     }
+
+    @Test
+    public void mergeSortTestNullDesc() {
+        Assert.assertArrayEquals(null, mergeSort.sort(null, MergeSort.SortOrder.DESC));
+    }
+
+
 }
